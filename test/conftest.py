41e3856cf2980f12be1a36125d587d080fc25796
Sanzida68 <sanzidamaisha20682gmail.com>
2024-01-03 13:07:07 +0600

    POM

--- test/conftest.py
+++ test/conftest.py
@@ -5,10 +5,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
-def driver():
+def driver(request):
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.implicitly_wait(10)
     driver.maximize_window()
+    request.cls.driver = driver
     yield driver
     driver.close()
     driver.quit()
--- test/test_signin.py
+++ test/test_signin.py
@@ -1,40 +1,31 @@
 import time
-
+import pytest
 from test.pages.HomePage import HomePage
 from test.pages.SignInPage import SigninPage
 
 
-def test_signin(driver):
-    HomePage(driver).for_signin_page("https://test.usehomeleads.com/")
-    signin_page = SigninPage(driver)
-    signin_page.enter_email("sanzidamaisha2068@gmail.com")
-    signin_page.enter_pass("123456")
-    signin_page.enter_signin()
-    time.sleep(3)
+@pytest.mark.usefixtures("driver")
+class Test_signin:
 
+    def test_signin(self):
+        HomePage(self).for_signin_page("https://test.usehomeleads.com/")
+        SigninPage(self).for_login("sanzidamaisha2068@gmail.com", "123456")
+        time.sleep(3)
 
-def invalid_email_valid_pass(driver):
-    HomePage(driver).for_signin_page("https://test.usehomeleads.com/")
-    signin_page = SigninPage(driver)
-    signin_page.enter_email("sanzidaafrin1000@gmail.com")
-    signin_page.enter_pass("123456")
-    signin_page.enter_signin()
-    time.sleep(3)
+    def invalid_email_valid_pass(self):
+        HomePage(self).for_signin_page("https://test.usehomeleads.com/")
+        SigninPage(self).for_login("sanzidaafrin1000@gmail.com", "123456")
+        time.sleep(3)
 
+    def valid_email_invalid_pass(self):
+        HomePage(self).for_signin_page("https://test.usehomeleads.com/")
+        SigninPage(self).for_login("sanzidamaisha2068@gmail.com", "1234567")
+        time.sleep(3)
 
-def valid_email_invalid_pass(driver):
-    HomePage(driver).for_signin_page("https://test.usehomeleads.com/")
-    signin_page = SigninPage(driver)
-    signin_page.enter_email("sanzidamaisha2068@gmail.com")
-    signin_page.enter_pass("1234567")
-    signin_page.enter_signin()
-    time.sleep(3)
-
-
-def empty_field(driver):
-    HomePage(driver).for_signin_page("https://test.usehomeleads.com/")
-    signin_page = SigninPage(driver)
-    signin_page.enter_email("")
-    signin_page.enter_pass("")
-    signin_page.enter_signin()
-    time.sleep(3)
+# def empty_field(driver):
+# HomePage(driver).for_signin_page("https://test.usehomeleads.com/")
+# signin_page = SigninPage(driver)
+# signin_page.enter_email("")
+# signin_page.enter_pass("")
+# signin_page.enter_signin()
+# time.sleep(3)
