1f096ec2a2fd03d99b5174cc130b547eae5eb449
Sanzida68 <sanzidamaisha20682gmail.com>
2024-01-03 09:43:07 +0600

    POM

---
+++ test/test_logout.py
@@ -0,0 +1,14 @@
+import time
+
+from test.pages.LogOutPage import LogoutPage
+from test.pages.SignInPage import SigninPage
+
+
+def test_logout(driver):
+    signin = SigninPage(driver)
+    signin.for_login("https://test.usehomeleads.com/", "sanzidamaisha2068@gmail.com", "123456")
+
+    logout = LogoutPage(driver)
+    logout.click_profile()
+    #logout.click_logout()
+    time.sleep(3)
