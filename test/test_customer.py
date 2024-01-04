495f2c53c3794bcf5c2596f4361a0b82774f4311
Sanzida68 <sanzidamaisha20682gmail.com>
2024-01-04 17:13:41 +0600

    POM

--- test/test_customer.py
+++ test/test_customer.py
@@ -14,3 +14,15 @@ class TestCustomer:
                                                cus_city=lead.cus_city, cus_postal=lead.cus_postal,
                                                cus_message=lead.cus_message)
         time.sleep(3)
+
+    def test_with_mandatory_field(self, driver):
+        CustomerPage(driver).for_customer_lead(url=lead.cus_url, cus_n=lead.cus_name, cus_email=lead.cus_mail,
+                                               cus_phone=lead.cus_phone, cus_address=lead.cus_address,
+                                               cus_city=lead.cus_city, cus_postal=lead.cus_postal,
+                                               cus_message="")
+
+    def test_without_mandatory_field(self, driver):
+        CustomerPage(driver).for_customer_lead(url=lead.cus_url, cus_n="", cus_email="",
+                                               cus_phone="", cus_address="",
+                                               cus_city="", cus_postal="",
+                                               cus_message="")
